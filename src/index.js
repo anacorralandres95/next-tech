78632d6d34c8da3a782d02f7fd31ad9441fde4f2
Ana Corral <anacorralandres@ied.edu>
2021-12-08 22:18:35 +0100

    Basic structure and configuration

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/fonts.css';
+import Products from './screens/Products';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Products />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
