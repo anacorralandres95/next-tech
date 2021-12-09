21e01e4839a00d5be827e8dc0c08553b05911e5d
Ana Corral <anacorralandres@ied.edu>
2021-12-10 00:12:44 +0100

    Brands filter

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -2,9 +2,24 @@ import React from "react";
 import { useProducts } from "./useProducts";
 
 const Products = () => {
-  const { products, searchValue, setSearchValue } = useProducts();
+  const {
+    products,
+    searchValue,
+    brands,
+    selectedModels,
+    setSearchValue,
+    setSelectedModels,
+    getIsVisibleProducts,
+  } = useProducts();
 
-  const onChangeSearchValue = (e) => setSearchValue(e.target.value);
+  const onChangeSearchValue = (e) => {
+    clearSelectedBrands();
+    setSearchValue(e.target.value);
+  };
+
+  const clearSearchValue = () => setSearchValue("");
+
+  const clearSelectedBrands = () => setSelectedModels([]);
 
   if (!products) <></>;
 
@@ -12,16 +27,48 @@ const Products = () => {
     <>
       <input
         type="text"
+        value={searchValue}
         onChange={onChangeSearchValue}
         placeholder="Buscar..."
         className="search"
       />
 
+      <div className="brands-container">
+        <small className="small">Filtra por modelos</small>
+
+        <div
+          className={`brand ${selectedModels.length === 0 && "brand-selected"}`}
+          onClick={clearSelectedBrands}
+        >
+          <small>Todos</small>
+        </div>
+
+        {brands?.map((brand, i) => {
+          const onSelectBrand = () => {
+            clearSearchValue();
+            if (selectedModels.includes(brand)) {
+              setSelectedModels(selectedModels.filter((s) => s !== brand));
+            } else {
+              setSelectedModels(selectedModels.concat(brand));
+            }
+          };
+
+          const isSelected = selectedModels.includes(brand);
+
+          return (
+            <div
+              key={i}
+              className={`brand ${isSelected && "brand-selected"}`}
+              onClick={onSelectBrand}
+            >
+              <small>{brand}</small>
+            </div>
+          );
+        })}
+      </div>
+
       {products?.map((product, i) => {
-        if (
-          product.brand.toLowerCase().includes(searchValue.toLowerCase()) ||
-          product.model.toLowerCase().includes(searchValue.toLowerCase())
-        )
+        if (getIsVisibleProducts(product))
           return (
             <div key={i}>
               <p>{product.brand}</p>
--- src/screens/useProducts.js
+++ src/screens/useProducts.js
@@ -5,6 +5,7 @@ export const useProducts = () => {
   const dispatch = useDispatch();
 
   const [searchValue, setSearchValue] = useState("");
+  const [selectedModels, setSelectedModels] = useState([]);
 
   const products = useSelector((state) => state.productsModel.products);
 
@@ -12,5 +13,39 @@ export const useProducts = () => {
     dispatch.productsModel.getProductsInfo();
   }, []);
 
-  return { products, searchValue, setSearchValue };
+  const brands = products
+    ?.map((p) => p.price && p.model.split(" ")[0])
+    .filter(function (item, pos, a) {
+      return item && a.indexOf(item) == pos;
+    });
+
+  const getIsVisibleProducts = (product) => {
+    if (product.price) {
+      if (!searchValue && selectedModels.length === 0) return true;
+
+      if (
+        searchValue &&
+        (product.brand.toLowerCase().includes(searchValue.toLowerCase()) ||
+          product.model.toLowerCase().includes(searchValue.toLowerCase())) &&
+        product.price
+      )
+        return true;
+
+      if (
+        selectedModels.length > 0 &&
+        selectedModels.includes(product.model.split(" ")[0])
+      )
+        return true;
+    }
+  };
+
+  return {
+    products,
+    searchValue,
+    brands,
+    selectedModels,
+    setSearchValue,
+    setSelectedModels,
+    getIsVisibleProducts,
+  };
 };
