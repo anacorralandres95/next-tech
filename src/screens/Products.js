6755f3d0d4c9264e9455e45239c256b2f4e4a334
Ana Corral <anacorralandres@ied.edu>
2021-12-09 23:30:15 +0100

    Search input

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,12 +1,34 @@
 import React from "react";
-// import { useProducts } from "./useProducts";
+import { useProducts } from "./useProducts";
 
 const Products = () => {
-  // const { products } = useProducts();
+  const { products, searchValue, setSearchValue } = useProducts();
+
+  const onChangeSearchValue = (e) => setSearchValue(e.target.value);
+
+  if (!products) <></>;
 
   return (
     <>
-      <p>Products</p>
+      <input
+        type="text"
+        onChange={onChangeSearchValue}
+        placeholder="Buscar..."
+        className="search"
+      />
+
+      {products?.map((product, i) => {
+        if (
+          product.brand.toLowerCase().includes(searchValue.toLowerCase()) ||
+          product.model.toLowerCase().includes(searchValue.toLowerCase())
+        )
+          return (
+            <div key={i}>
+              <p>{product.brand}</p>
+              <p>{product.model}</p>
+            </div>
+          );
+      })}
     </>
   );
 };
--- src/screens/useProducts.js
+++ src/screens/useProducts.js
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export const useProducts = () => {
   const dispatch = useDispatch();
+
+  const [searchValue, setSearchValue] = useState("");
 
   const products = useSelector((state) => state.productsModel.products);
 
@@ -10,5 +12,5 @@ export const useProducts = () => {
     dispatch.productsModel.getProductsInfo();
   }, []);
 
-  return { products };
+  return { products, searchValue, setSearchValue };
 };
