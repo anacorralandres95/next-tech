87319929a7d262c46e802d8ca49e282cd3ed496b
Ana Corral <anacorralandres@ied.edu>
2021-12-11 16:50:11 +0100

    Responsive

--- src/components/common/layout/nav-bar/NavBar.js
+++ src/components/common/layout/nav-bar/NavBar.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import cart from "../../../../assets/icons/cart.png";
+import home from "../../../../assets/icons/home.png";
 
 const NavBar = () => {
   const { singularProduct, counter } = useSelector(
     (state) => state.productsModel
   );
 
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const setWidth = () => {
+      const { innerWidth: width } = window;
+      setScreenWidth(width);
+    };
+
+    window.addEventListener("resize", setWidth);
+
+    return () => {
+      window.removeEventListener("resize", setWidth);
+    };
+  }, []);
+
   return (
     <div className="nav-bar">
-      <div className="products">
-        <a href="/">Productos</a>
-        {!singularProduct && <div className="bar" />}
-        {singularProduct && (
-          <>
-            <p className="divider">·</p>
-            <p>{singularProduct.model}</p>
-          </>
-        )}
-      </div>
+      {screenWidth > 900 ? (
+        <div className="products">
+          <a href="/">Productos</a>
+          {!singularProduct && <div className="bar" />}
+          {singularProduct && (
+            <>
+              <p className="divider">·</p>
+              <p>{singularProduct.model}</p>
+            </>
+          )}
+        </div>
+      ) : (
+        <a href="/" className="products">
+          <img src={home} className="nav-icon" />
+        </a>
+      )}
       <div>
         <a href="/">
           <span className="logo-1">Next</span>
@@ -26,7 +48,7 @@ const NavBar = () => {
         </a>
       </div>
       <div className="cart">
-        <img src={cart} className="cart-icon" />
+        <img src={cart} className="nav-icon" />
         <div className="counter">
           <small>{counter ?? 0}</small>
         </div>
